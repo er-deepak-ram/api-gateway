c6c464297de90ce90db1cb773fb8640be664a630
Deepak Ram <deepak.kumar2305@gmail.com>
2022-07-03 17:27:10 +0530

    API Gateway for Banking

---
+++ src/test/java/com/googly/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googly.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
